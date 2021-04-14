0133cd9a10c8e87e8303453cea24936f4f70bdb3
Rafael Ventura <raf4ael@gmail.com>
2021-04-13 23:48:54 -0300

    componentizando o Header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import "./App.css";
-import { FaFacebook, FaSearch, FaAlignJustify } from "react-icons/fa";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      {/* HEADER
-      STORIES 
-      POST FEED 
-      FEED  */}
-      <div className="header">
-        <div className="headerLeft">
-          <div className="logo-fb">
-            <FaFacebook />
-          </div>
-          <div className="search-fb">
-            <FaSearch />
-          </div>
-          <div className="menu-fb">
-            <FaAlignJustify />
-          </div>
-        </div>
-        <div className="headerRight">
-          <div className="plus-btn">+</div>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import { FaFacebook, FaSearch, FaAlignJustify } from "react-icons/fa";
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerLeft">
+        <div className="logo-fb">
+          <FaFacebook />
+        </div>
+        <div className="search-fb">
+          <FaSearch />
+        </div>
+        <div className="menu-fb">
+          <FaAlignJustify />
+        </div>
+      </div>
+      <div className="headerRight">
+        <div className="plus-btn">+</div>
+      </div>
+    </div>
+  );
+}
