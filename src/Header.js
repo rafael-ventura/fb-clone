d705af5a59f55f8aa75917a27174e7019a268a1b
Rafael Ventura <raf4ael@gmail.com>
2021-04-14 00:16:31 -0300

    novos icones no header

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,8 @@
 import { FaFacebook, FaSearch, FaAlignJustify } from "react-icons/fa";
+import { AiFillHome, AiOutlineShop } from "react-icons/ai";
+import { RiFlag2Line } from "react-icons/ri";
+import { MdOndemandVideo } from "react-icons/md";
+
 export default function Header() {
   return (
     <div className="header">
@@ -8,6 +12,18 @@ export default function Header() {
         </div>
         <div className="search-fb">
           <FaSearch />
+        </div>
+        <div className="home-fb">
+          <AiFillHome />
+        </div>
+        <div className="flag-fb">
+          <RiFlag2Line />
+        </div>
+        <div className="live-fb">
+          <MdOndemandVideo />
+        </div>
+        <div className="shop-fb">
+          <AiOutlineShop />
         </div>
         <div className="menu-fb">
           <FaAlignJustify />
